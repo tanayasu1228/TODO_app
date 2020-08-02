b090414bf924f7daa7dc3b9ca437b615abe7762a
tanahashiyasuhiro <tana.yasu1228@gmail.com>
2020-08-02 23:12:01 +0900

    リファクタリングをしました

--- main.rb
+++ main.rb
@@ -1,23 +1,36 @@
 require 'trello'
 require 'pry'
 
+def puts_todos
+  configure
+  cards = get_cards
+
+  # 出力
+  puts "========================="
+  puts "【TODO】"
+  cards.each do |card|
+    puts card.name
+  end
+  puts "========================="
+end
+
 # 認証
-Trello.configure do |config|
-  config.developer_public_key = 'TRELLO_DEVELOPER_PUBLIC_KEY'
-  config.member_token = 'TRELLO_MEMBER_TOKEN'
+def configure
+
+  Trello.configure do |config|
+    config.developer_public_key = 'TRELLO_DEVELOPER_PUBLIC_KEY'
+    config.member_token = 'TRELLO_MEMBER_TOKEN'
+  end
 end
 
 # カードの取得
-member = Trello::Member.find('TrelloのユーザーID')
-boards = member.boards
-board = boards.find { |board| board.name == 'TODO' }
-list = board.lists[0]
-cards = list.cards
-
-# 出力
-puts "========================="
-puts "【TODO】"
-cards.each do |card|
-  puts card.name
+def get_cards
+  member = Trello::Member.find('TrelloのユーザーID')
+  boards = member.boards
+  board = boards.find { |board| board.name == 'TODO' }
+  list = board.lists[0]
+  cards = list.cards
+  cards
 end
-puts "========================="
+
+puts_todos
